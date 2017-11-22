49959b2e1bfffe755cf213210109c4ced1f84e89
zhoupengzu <zhoupengzu@maizijf.com>
2017-11-22 14:10:07 +0800

    创建工程

---
+++ ZPZBlockPractice/ZPZBlockPractice/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  ZPZBlockPractice
+//
+//  Created by zhoupengzu on 2017/11/22.
+//  Copyright © 2017年 zhoupengzu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

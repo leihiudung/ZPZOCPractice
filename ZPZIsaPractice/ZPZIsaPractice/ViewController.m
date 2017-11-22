90a1fa986106cf93c3cab577edfb27a1985cb1c5
zhoupengzu <zhoupengzu@maizijf.com>
2017-11-22 14:57:44 +0800

    创建工程

---
+++ ZPZIsaPractice/ZPZIsaPractice/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  ZPZIsaPractice
+//
+//  Created by zhoupengzu on 2017/11/22.
+//  Copyright © 2017年 zhoupengzu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

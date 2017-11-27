b852e1103ae9cddb70b7ddbf67f38276f5a62740
zhoupengzu <zhoupengzu@maizijf.com>
2017-11-27 15:09:47 +0800

    local block test

--- ZPZBlockPractice/ZPZBlockPractice/ViewController.m
+++ ZPZBlockPractice/ZPZBlockPractice/ViewController.m
@@ -10,7 +10,18 @@
 #import <objc/runtime.h>
 #import "ZPZIsaTestModel.h"
 
+typedef void(^testBlock)();
+typedef void(^testBlock1)(int a);
+
 @interface ViewController ()
+
+@property (nonatomic,strong) testBlock strongBlock;
+@property (nonatomic,copy) testBlock copyBlock;
+@property (nonatomic,weak) testBlock weakBlock;
+
+@property (nonatomic,strong) testBlock1 strongBlock1;
+@property (nonatomic,copy) testBlock1 copyBlock1;
+@property (nonatomic,weak) testBlock1 weakBlock1;
 
 @end
 
@@ -20,8 +31,9 @@
     [super viewDidLoad];
     self.edgesForExtendedLayout = UIRectEdgeNone;
     self.view.backgroundColor = [UIColor orangeColor];
-//    [self defineNormalLocalBlock];
-    [self getObjectIsa];
+//    [self defineNormalLocalBlock];  //不引用外部变量
+//    [self getObjectIsa];
+    [self defineBlockWithOutLocalParams];
 }
 
 /**
@@ -33,16 +45,31 @@
     Class cls = [ZPZIsaTestModel class];
     NSLog(@"%@",object_getClass(cls));  //ZPZIsaTestModel
 }
-
+/**
+ * 不引用外部变量的时候，其类型为全局的block
+ */
 - (void)defineNormalLocalBlock {
     void(^localBlock)(void) = ^{
         NSLog(@"localBlock");
     };
-    NSLog(@"%@",object_getClass(localBlock));
     Class cls = object_getClass(localBlock);
-    NSLog(@"%@",class_getSuperclass(cls));
-    NSLog(@"%@",object_getClass(cls));
+    NSLog(@"%@",cls); //__NSGlobalBlock__
+    NSLog(@"%@",class_getSuperclass(cls));  //__NSGlobalBlock
+    NSLog(@"%@",object_getClass(cls)); //__NSGlobalBlock__ ，这是元类
     
+}
+/**
+ * 引用外部变量的时候，变成了堆上的block
+ */
+- (void)defineBlockWithOutLocalParams{
+    /*__block*/ int a = 10;
+    void(^localBlock)(void) = ^{
+        NSLog(@"%@",@(a));
+    };
+    Class cls = object_getClass(localBlock);
+    NSLog(@"%@",cls); //__NSMallocBlock__
+    NSLog(@"%@",class_getSuperclass(cls)); //__NSMallocBlock
+    NSLog(@"%@",object_getClass(cls)); //__NSMallocBlock__
 }
 
 

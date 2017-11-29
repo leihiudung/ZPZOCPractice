a0cd2c2374fe557473640ef5fb032b0bc20d9533
zhoupengzu <zhoupengzu@maizijf.com>
2017-11-29 10:17:07 +0800

    创建消息传递工程

---
+++ ZPZMessageSendPractice/ZPZMessageSendPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZPZMessageSendPractice
+//
+//  Created by zhoupengzu on 2017/11/29.
+//  Copyright © 2017年 zhoupengzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZPZMessageSendPractice/ZPZMessageSendPractice/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  ZPZMessageSendPractice
+//
+//  Created by zhoupengzu on 2017/11/29.
+//  Copyright © 2017年 zhoupengzu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

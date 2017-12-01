e327d2db0ab8379c3087fa0d5b1441312e439c50
zhoupengzu <zhoupengzu@maizijf.com>
2017-12-01 09:39:45 +0800

    创建UITextView的探索工程

---
+++ ZPZTextViewPractice/ZPZTextViewPracticeTests/ZPZTextViewPracticeTests.m
@@ -0,0 +1,39 @@
+//
+//  ZPZTextViewPracticeTests.m
+//  ZPZTextViewPracticeTests
+//
+//  Created by zhoupengzu on 2017/12/1.
+//  Copyright © 2017年 zhoupengzu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZPZTextViewPracticeTests : XCTestCase
+
+@end
+
+@implementation ZPZTextViewPracticeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

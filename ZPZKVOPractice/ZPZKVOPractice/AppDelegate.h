aa12496166522918e55fa223691d204e955c049f
zhoupengzu <zhoupengzu@maizijf.com>
2017-12-07 10:16:29 +0800

    创建探索KVO的工程

---
+++ ZPZKVOPractice/ZPZKVOPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZPZKVOPractice
+//
+//  Created by zhoupengzu on 2017/12/7.
+//  Copyright © 2017年 zhoupengzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

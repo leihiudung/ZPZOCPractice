c95af75a95cf3adb2ab7565492021520daa1f3d1
zhoupengzu <zhoupengzu@maizijf.com>
2017-12-14 14:37:05 +0800

    创建工程

---
+++ ZPZAlertViewController/ZPZAlertViewController/ZPZAlertView.h
@@ -0,0 +1,13 @@
+//
+//  ZPZAlertView.h
+//  ZPZAlertViewController
+//
+//  Created by zhoupengzu on 2017/12/13.
+//  Copyright © 2017年 zhoupengzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZPZAlertView : UIView
+
+@end
---
+++ ZPZAlertViewController/ZPZAlertViewController/ZPZAlertView.m
@@ -0,0 +1,21 @@
+//
+//  ZPZAlertView.m
+//  ZPZAlertViewController
+//
+//  Created by zhoupengzu on 2017/12/13.
+//  Copyright © 2017年 zhoupengzu. All rights reserved.
+//
+
+#import "ZPZAlertView.h"
+
+@implementation ZPZAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

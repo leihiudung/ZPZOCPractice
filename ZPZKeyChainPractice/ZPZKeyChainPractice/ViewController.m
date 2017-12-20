e9a996287dfab28c341b95b86d24093ebdafca17
zhoupengzu <zhoupengzu@maizijf.com>
2017-12-20 09:36:00 +0800

    创建Keychain工程

---
+++ ZPZKeyChainPractice/ZPZKeyChainPractice/ViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  ZPZKeyChainPractice
+//
+//  Created by zhoupengzu on 2017/12/19.
+//  Copyright © 2017年 zhoupengzu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+//    [self configUI];
+}
+
+//- (void)configUI {
+//    CGFloat margin = 15;
+//    CGFloat width = [UIScreen mainScreen].bounds.size.width - 2 * margin;
+//    CGFloat height = 50;
+//    UIColor * backColor = [UIColor orangeColor];
+//    UIFont * titleFont = [UIFont systemFontOfSize:15];
+//    CGFloat beginY = 20;
+//
+//}
+
+- (void)saveBasicInfo {
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

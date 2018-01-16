1f5d64b3d87a04f805151a1365ff66da559294b9
zhoupengzu <zhoupengzu@maizijf.com>
2018-01-16 17:36:19 +0800

    test

---
+++ ZPZThirdFrameworkPractice/Pods/Target Support Files/Pods-ZPZThirdFrameworkPractice/Pods-ZPZThirdFrameworkPractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZPZThirdFrameworkPractice : NSObject
+@end
+@implementation PodsDummy_Pods_ZPZThirdFrameworkPractice
+@end
---
+++ ZPZThirdFrameworkPractice/Pods/Target Support Files/Pods-ZPZThirdFrameworkPracticeTests/Pods-ZPZThirdFrameworkPracticeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZPZThirdFrameworkPracticeTests : NSObject
+@end
+@implementation PodsDummy_Pods_ZPZThirdFrameworkPracticeTests
+@end

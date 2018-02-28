15b9a75179c507c4fa2c56fd0c3a416e4d0a81c1
zhoupengzu <zhoupengzu@maizijf.com>
2018-02-28 15:22:53 +0800

    编辑内容

--- ZPZPhotoPractice/ZPZPhotoPractice/ZPZAssetChangeRequestViewController.m
+++ ZPZPhotoPractice/ZPZPhotoPractice/ZPZAssetChangeRequestViewController.m
@@ -12,7 +12,9 @@
 @interface ZPZAssetChangeRequestViewController ()
 {
     NSMutableArray * identifierArr;
+    PHAsset * myAsset;
 }
+@property (weak, nonatomic) IBOutlet UIImageView *finalImageView;
 
 @end
 
@@ -95,24 +97,90 @@
         }
     }];
 }
+
 - (IBAction)toEditAssetContent:(id)sender {
-    [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
-        PHFetchResult<PHAsset *> * fetchResult = [PHAsset fetchAssetsWithLocalIdentifiers:identifierArr options:nil];
-        if (fetchResult.count > 0) {
-            PHAsset * firstAsset = fetchResult.firstObject;
-            PHAssetChangeRequest * changeRequest = [PHAssetChangeRequest changeRequestForAsset:firstAsset];
-            [firstAsset requestContentEditingInputWithOptions:nil completionHandler:^(PHContentEditingInput * _Nullable contentEditingInput, NSDictionary * _Nonnull info) {
-                PHContentEditingOutput * output = [[PHContentEditingOutput alloc] initWithContentEditingInput:contentEditingInput];
-                
+    // 1、根据identifier查找到asset
+    PHFetchResult<PHAsset *> * fetchResult = [PHAsset fetchAssetsWithLocalIdentifiers:identifierArr options:nil];
+    if (fetchResult.count > 0) {
+        PHAsset * firstAsset = fetchResult.firstObject;
+        myAsset = firstAsset;
+        if ([firstAsset canPerformEditOperation:PHAssetEditOperationContent]) {
+            NSLog(@"可以被修改");
+        }
+        // 2、获取PHContentEditingInput对象
+        PHContentEditingInputRequestOptions * requestOptions = [[PHContentEditingInputRequestOptions alloc] init];
+        // 该方法只有在已经存在PHAdjustmentData的情况下才会被调用
+        requestOptions.canHandleAdjustmentData = ^BOOL(PHAdjustmentData * _Nonnull adjustmentData) {
+            NSLog(@"formatIdentifier:%@,formatVersion:%@", adjustmentData.formatIdentifier, adjustmentData.formatVersion);
+            return YES;
+        };
+//        PHContentEditingInputRequestID requestId =
+        [firstAsset requestContentEditingInputWithOptions:requestOptions completionHandler:^(PHContentEditingInput * _Nullable contentEditingInput, NSDictionary * _Nonnull info) {
+            // 3、创建PHAdjustmentdata，可以在data参数里设置要做的操作，比如过滤等，可以是一个序列串，但是这里的长度是有限制的
+            PHAdjustmentData * adjustData = [[PHAdjustmentData alloc] initWithFormatIdentifier:firstAsset.localIdentifier formatVersion:@"1.0" data:[@"ceshi" dataUsingEncoding:NSUTF8StringEncoding]];
+            // 4、初始化PHContentEditingOutput对象
+            PHContentEditingOutput * output = [[PHContentEditingOutput alloc] initWithContentEditingInput:contentEditingInput];
+            output.adjustmentData = adjustData;
+            // 通过PHContentEditingInput可以获取到想要的信息，处理图片:修改图片整体颜色
+            NSURL * imageUrl = [contentEditingInput fullSizeImageURL];  // 包含图片数据的文件
+            NSData * originalImageData = [NSData dataWithContentsOfURL:imageUrl];
+            UIImage * originalImage = [UIImage imageWithData:originalImageData];
+            CGImageRef imageRef = originalImage.CGImage;
+            size_t width = CGImageGetWidth(imageRef);
+            size_t height = CGImageGetHeight(imageRef);
+            size_t bitsPerComponent = CGImageGetBitsPerComponent(imageRef);
+            size_t bytesPerRow = CGImageGetBytesPerRow(imageRef);
+            uint32_t bitmapInfo = CGImageGetBitmapInfo(imageRef);
+            CGColorSpaceRef space = CGImageGetColorSpace(imageRef);
+            //获取每个像素的字节数据集合
+            CGDataProviderRef dataProviderRef = CGImageGetDataProvider(imageRef);
+            CFDataRef dataRef = CGDataProviderCopyData(dataProviderRef);
+            CFMutableDataRef mutableDataRef = CFDataCreateMutableCopy(kCFAllocatorDefault, 0, dataRef);
+            //                const UInt8 * constBytesPtr = CFDataGetBytePtr(dataRef);
+            UInt8 * bytesPtr = CFDataGetMutableBytePtr(mutableDataRef);
+            NSUInteger length = CFDataGetLength(mutableDataRef);
+            // 一个像素由四个字节byte组成
+            for (int i = 0; i < length; i += 4) {
+                UInt8 red = bytesPtr[i];
+                UInt8 green = bytesPtr[i + 1];
+                UInt8 blue = bytesPtr[i + 2];
+                UInt8 average = (red + green + blue) / 3;
+                bytesPtr[i] = average;
+                bytesPtr[i + 1] = average;
+                bytesPtr[i + 2] = average;
+            }
+            CGContextRef imageContextRef = CGBitmapContextCreate(bytesPtr, width, height, bitsPerComponent, bytesPerRow, space, bitmapInfo);
+            CGImageRef finalImageRef = CGBitmapContextCreateImage(imageContextRef);
+            UIImage * finalImage = [UIImage imageWithCGImage:finalImageRef];
+            _finalImageView.image = finalImage;
+            // ⚠️这里为什么一定要用UIImageJPEGRepresentation才能成功，而UIImagePNGRepresentation会失败，生成的数据是正确的，但是相册相应的资源无法更新???
+            // 因为这个和renderedContentURL有关，图片的输出只能是JPEG格式，video只能是.mov格式输出
+            // 对于Live Photo，调用saveLivePhotoToOutput:options:completionHandler:方法保存和更新
+            BOOL isSuccess = [UIImageJPEGRepresentation(finalImage, 1) writeToURL:output.renderedContentURL atomically:NO];
+            if (isSuccess) {
+                NSLog(@"写入成功");
+            } else {
+                NSLog(@"写入失败");
+            }
+            CGImageRelease(finalImageRef);
+            CGContextRelease(imageContextRef);
+            CFRelease(mutableDataRef);
+            CFRelease(dataRef);
+            
+            // 5、赋值更新
+            [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+                // 使用PHAssetChangeRequest
+                PHAssetChangeRequest * changeRequest = [PHAssetChangeRequest changeRequestForAsset:myAsset];
+                changeRequest.contentEditingOutput = output;
+            } completionHandler:^(BOOL success, NSError * _Nullable error) {
+                if (success) {
+                    NSLog(@"编辑内容成功");
+                } else {
+                    NSLog(@"编辑内容失败:%@", error);
+                }
             }];
-        }
-    } completionHandler:^(BOOL success, NSError * _Nullable error) {
-        if (success) {
-            NSLog(@"编辑内容成功");
-        } else {
-            NSLog(@"编辑内容失败");
-        }
-    }];
+        }];
+    }
 }
 
 @end

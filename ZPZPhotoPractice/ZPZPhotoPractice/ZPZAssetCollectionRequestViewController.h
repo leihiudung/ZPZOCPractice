4d2aac5da50ce4c924237ba59c7bdd2463db660a
zhoupengzu <zhoupengzu@maizijf.com>
2018-03-01 10:22:55 +0800

    添加创建相册入口

---
+++ ZPZPhotoPractice/ZPZPhotoPractice/ZPZAssetCollectionRequestViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZPZAssetCollectionRequestViewController.h
+//  ZPZPhotoPractice
+//
+//  Created by zhoupengzu on 2018/3/1.
+//  Copyright © 2018年 zhoupengzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZPZAssetCollectionRequestViewController : UIViewController
+
+@end
---
+++ ZPZPhotoPractice/ZPZPhotoPractice/ZPZAssetCollectionRequestViewController.m
@@ -0,0 +1,24 @@
+//
+//  ZPZAssetCollectionRequestViewController.m
+//  ZPZPhotoPractice
+//
+//  Created by zhoupengzu on 2018/3/1.
+//  Copyright © 2018年 zhoupengzu. All rights reserved.
+//
+
+#import "ZPZAssetCollectionRequestViewController.h"
+
+@interface ZPZAssetCollectionRequestViewController ()
+
+@end
+
+@implementation ZPZAssetCollectionRequestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)toCreateAssetCollection:(id)sender {
+}
+
+@end
